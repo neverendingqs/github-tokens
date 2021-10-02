7a8a757b18993881945bf4169369d64cd306f147
Mark Tse <mark.tse@neverendingqs.com>
2021-10-01 21:49:11 -0400

    feat(generator): now creates token for single repo on run.

---
+++ src/index.mjs
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Octokit } from 'octokit';
+import { createAppAuth } from '@octokit/auth-app';
+import sodium from 'tweetsodium';
+
+const org = 'neverendingqs';
+
+const installationId = process.env.GH_INSTALLATION_ID;
+
+const octokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: process.env.GH_APP_ID,
+    privateKey: process.env.GH_PRIVATE_KEY,
+    installationId,
+  },
+});
+
+async function createOrUpdateRepoSecret({ repo, secret }) {
+  const { data: { key_id, key } } = await octokit.rest.actions.getRepoPublicKey({
+    owner: org,
+    repo,
+  });
+
+  // Convert the message and key to Uint8Array's (Buffer implements that interface)
+  const messageBytes = Buffer.from(secret.value);
+  const keyBytes = Buffer.from(key, 'base64');
+
+  // Encrypt using LibSodium.
+  const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+
+  return octokit.rest.actions.createOrUpdateRepoSecret({
+    owner: org,
+    repo,
+    secret_name: secret.key,
+    encrypted_value: Buffer.from(encryptedBytes).toString('base64'),
+    key_id
+  });
+}
+
+const { data: { token }} = await octokit.rest.apps.createInstallationAccessToken({
+  installation_id: installationId,
+});
+
+const response = await createOrUpdateRepoSecret({
+  repo: 'serverless-dotenv-example',
+  secret: {
+    key: 'REPO_GITHUB_TOKEN',
+    value: token
+  }
+});
+
+console.log(response);
